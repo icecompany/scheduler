f410725d28cb8aa1b41086afe26732e4c13c7126
xakepok@xakepok.com <44duP_yx579>
2020-06-07 04:51:20 +0300

    Set trigger check

--- administrator/components/com_scheduler/scheduler.php
+++ administrator/components/com_scheduler/scheduler.php
@@ -15,6 +15,9 @@ require_once JPATH_ADMINISTRATOR . "/components/com_prj/helpers/prj.php";
 require_once JPATH_ADMINISTRATOR . "/components/com_mkv/helpers/mkv.php";
 require_once JPATH_ADMINISTRATOR . "/components/com_contracts/helpers/contracts.php";
 require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/scheduler.php';
+$db = JFactory::getDbo();
+$db->setQuery("set @TRIGGER_CHECKS=true")->execute();
+
 
 // Execute the task
 $controller = BaseController::getInstance('scheduler');
