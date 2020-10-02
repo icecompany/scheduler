63a813e54d6bb53830ea223cf56191bca03a573c
xakepok@xakepok.com <44duP_yx579>
2020-10-02 15:20:19 +0300

    Fix send form

--- media/com_scheduler/js/script.js
+++ media/com_scheduler/js/script.js
@@ -4,9 +4,3 @@ function updateTask(taskID) {
     let url = encodeURI(`index.php?option=com_scheduler&task=task.updateTask&id=${taskID}&result=${result}`);
     location.href = url;
 }
-
-window.onload = function () {
-    jQuery("#adminForm").on('submit', function (e) {
-       e.preventDefault();
-    });
-}
